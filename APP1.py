d1528ba7b0c77ec8392f85097dcf1766eacd87ba
Masoumeh-Hashemi <masoomeh.hashemi92@gmail.com>
2019-06-14 12:56:49 +0430

    creare session id

--- APP1.py
+++ APP1.py
@@ -6,9 +6,14 @@
 import socket
 import DB
 import LAYER7
+import time
+#TODO:how to import these two whrandom,md5
+# import whrandom
+# import md5
+import datetime
+
 
 # Define constrains
-
 host = "127.0.0.1"
 port = 8081
 
@@ -33,12 +38,30 @@ class app:
         
         #should return bool ,appcode
         return app.app_code
-    
+
     def create_session_id(self):
-        # TODO:sessionid
-        pass
+        #TODO:make sessionId more complicated
+        now = datetime.datetime.now()
+        sentence=now.__str__()
+        sentence = ''.join(sentence.split())
+        return sentence
+
+        #TODO:"""Build a new Session ID"""
+        # def getNewSID(self,tag):
+            # t1 = time.time()
+            # time.sleep( whrandom.random() )
+            # t2 = time.time()
+            # base = md5.new( tag + str(t1 +t2) )
+            # sid = tag + '_' + base.hexdigest()
+            # return sid
 
 
+
+	
+
+app1=app("golestan")
+result=app1.create_session_id()
+print (result)
 # app1=app("app1")
 # app1.handshake("app1","TODO:wait to function register_app")
 
@@ -46,4 +69,4 @@ def just_print(input):
     if input[0]=="app1":
      return "Im working"
 # app_socket.start_listening(just_print)
-app_socket.send_request("register_app golestanapp www...com",8080)
+app_socket.send_request(app1.create_session_id(),8080)
